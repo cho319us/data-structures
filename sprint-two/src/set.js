fee4ef1ffb5a7a3a6cdf0a94e346a2f425b089f1
Shirley Wu <shirleyswwu@gmail.com>
2020-07-03 16:32:04 -0700

    completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,39 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
-
+/*
+set = {
+  _storage: { Susan Sarandon : 'Susan Sarandon' }
+}
+*/
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // if item doesn't exist in storage
+  if (!this._storage[item]) {
+    // add item property to storage
+    this._storage[item] = item;
+  }
 };
 
+// return boolean
 setPrototype.contains = function(item) {
+  // if the storage has item property, then return true else return false
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item) {
+  // if item exist in storage, then delete item
+  if (this._storage[item]) {
+    delete this._storage[item];
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * add - O(1)
+ * contains - O(1)
+ * remove - O(1)
  */
